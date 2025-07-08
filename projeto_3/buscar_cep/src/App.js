a698da7f7248ad1592332965d9744dd9560bd51c
unknown <ivandelucca123@gmail.com>
2025-07-07 22:23:58 -0300

    Criando um componente

--- projeto_3/buscar_cep/src/App.js
+++ projeto_3/buscar_cep/src/App.js
@@ -1,11 +1,24 @@
+// Importação importante para a gestão de dados do projeto (biblioteca useState) !
+import { useState } from 'react';
+
+// importação da nossa API Via CEP!
+import api from './api/api-cep';
+
+// Importação da biblioteca de icones do react
+import { FiSearch } from 'react-icons/fi';
+
+// Importação do componente do logo tipo do governo federal
+import Logotipo_BR_CPN from './components/logotipo_br';
+
 import './css/style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Buscar CEP</h1>
-       <h2>Sistema de busca instantânea de endereços nível Brasil</h2>  
+        <Logotipo_BR_CPN/>
+        <h1>Buscar CEP</h1>
+        <h2>Sistema de busca instantânea de endereços nível Brasil</h2>  
       </header>
     </div>
   );
---
+++ projeto_3/buscar_cep/src/components/logotipo_br.jsx
@@ -0,0 +1,15 @@
+// Componentes do react.js, o seu formato deve ser sempre em .jsx
+// Sempre importe a biblioteca ou arquivo antes de qualquer processo no código
+import React from "react";
+import Logotipo_BR_IMG from '../img/logotipobr.jpeg';
+
+const Logotipo_BR_CPN = () => {
+    return(
+        <div>
+            <img src={Logotipo_BR_IMG} alt="Governo Federal!" title="Governo Federal!"/>
+        </div>
+    )
+}
+
+// Exporta o componente para o uso de qualquer outra camada da aplicação (app.js)!
+export default Logotipo_BR_CPN;
